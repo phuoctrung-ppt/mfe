77f738605115cdb9a9a24fd836cef9cc4fadedb9
phuoctrung-ppt <phuoctrung.ppt@gmail.com>
2024-03-06 14:13:01 +0700

    update text

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ isSignedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App
+            App MFE
           </Typography>
           <Button
             color="primary"
